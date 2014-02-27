10a7f178f8120a487eca0fdbeac70521396815dc
Jesus Rodriguez <Foxandxss@gmail.com>
2014-02-27 15:03:48 +0100

    Another travis fix

--- Gruntfile.js
+++ Gruntfile.js
@@ -57,7 +57,6 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-less');
-  grunt.loadNpmTasks('grunt-contrib-testem');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
